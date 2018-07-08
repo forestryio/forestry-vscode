4ad669b2621b454fb0fd70cc82e300fbe7633b30
ncphillips <ncphillips.19@gmail.com>
2018-07-08 00:55:03 -0300

    Display the section of the current page in the status bar

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,82 @@
-'use strict';
+"use strict";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
+import * as yaml from "yamljs";
 
+let site: Forestry.Site;
+let sectionStatusBarItem = vscode.window.createStatusBarItem(
+  vscode.StatusBarAlignment.Left
+);
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "forestry" is now active!');
+  site = new Forestry.Site(vscode.workspace.rootPath || "");
+  if (vscode.window.activeTextEditor) {
+    updateSectionStatus(vscode.window.activeTextEditor);
+  }
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "forestry" is now active!');
+  vscode.window.onDidChangeActiveTextEditor(editor => {
+    if (editor) {
+      updateSectionStatus(editor);
+    }
+  });
+}
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+function updateSectionStatus(editor: vscode.TextEditor) {
+  const section = site.sectionForUri(editor.document.uri);
+  if (section) {
+    sectionStatusBarItem.text = `Forestry.io Section: ${section.label}`;
+    sectionStatusBarItem.show();
+  } else {
+    sectionStatusBarItem.hide();
+  }
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
+export function deactivate() {}
+
+namespace Forestry {
+  interface Section {
+    path: string;
+    label: string;
+    create: "all" | "documents" | "none";
+    templates?: string[];
+  }
+  export class Site {
+    sections: Section[] = [];
+
+    constructor(private rootPath: string) {
+      this._loadForestrySettings();
+    }
+
+    sectionForUri(uri: vscode.Uri) {
+      const relPath = uri.path.replace(this.rootPath + "/", "");
+      const section = this.sections.find(section =>
+        relPath.startsWith(section.path)
+      );
+      if (section) {
+        return section;
+      }
+    }
+
+    private _loadForestrySettings() {
+      const settingsPath = path.join(
+        this.rootPath,
+        "src",
+        ".forestry",
+        "settings.yml"
+      );
+      const settingsFile = fs.readFileSync(settingsPath);
+      if (settingsFile) {
+        const settings = yaml.parse(settingsFile.toString());
+        this.sections = settings.sections || [];
+      }
+    }
+  }
 }
