417dc648288d16e8104e40ed134f9ca816eb3734
ncphillips <ncphillips.19@gmail.com>
2018-07-08 09:34:34 -0300

    split things up a bit

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import * as fs from "fs";
-import * as path from "path";
-import * as yaml from "yamljs";
+import { Forestry } from "./forestry";
 
 let site: Forestry.Site;
 let sectionStatusBarItem = vscode.window.createStatusBarItem(
@@ -16,67 +14,32 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "forestry" is now active!');
-  site = new Forestry.Site(vscode.workspace.rootPath || "");
-  if (vscode.window.activeTextEditor) {
-    updateSectionStatus(vscode.window.activeTextEditor);
-  }
 
-  vscode.window.onDidChangeActiveTextEditor(editor => {
-    if (editor) {
-      updateSectionStatus(editor);
-    }
-  });
+  // TODO: Add to .vscode workspace settings?
+  const pathToForestryDir = "src/.forestry";
+
+  site = new Forestry.Site(vscode.workspace.rootPath || "", pathToForestryDir);
+
+  updateSectionStatus(vscode.window.activeTextEditor);
+
+  let disposableSectionStatusUpdate = vscode.window.onDidChangeActiveTextEditor(
+    updateSectionStatus
+  );
+
+  context.subscriptions.push(disposableSectionStatusUpdate);
 }
 
-function updateSectionStatus(editor: vscode.TextEditor) {
-  const section = site.sectionForUri(editor.document.uri);
-  if (section) {
-    sectionStatusBarItem.text = `Forestry.io Section: ${section.label}`;
-    sectionStatusBarItem.show();
-  } else {
-    sectionStatusBarItem.hide();
+function updateSectionStatus(editor?: vscode.TextEditor) {
+  if (editor) {
+    const section = site.sectionForUri(editor.document.uri);
+    if (section) {
+      sectionStatusBarItem.text = `Forestry.io Section: ${section.label}`;
+      sectionStatusBarItem.show();
+    } else {
+      sectionStatusBarItem.hide();
+    }
   }
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
-
-namespace Forestry {
-  interface Section {
-    path: string;
-    label: string;
-    create: "all" | "documents" | "none";
-    templates?: string[];
-  }
-  export class Site {
-    sections: Section[] = [];
-
-    constructor(private rootPath: string) {
-      this._loadForestrySettings();
-    }
-
-    sectionForUri(uri: vscode.Uri) {
-      const relPath = uri.path.replace(this.rootPath + "/", "");
-      const section = this.sections.find(section =>
-        relPath.startsWith(section.path)
-      );
-      if (section) {
-        return section;
-      }
-    }
-
-    private _loadForestrySettings() {
-      const settingsPath = path.join(
-        this.rootPath,
-        "src",
-        ".forestry",
-        "settings.yml"
-      );
-      const settingsFile = fs.readFileSync(settingsPath);
-      if (settingsFile) {
-        const settings = yaml.parse(settingsFile.toString());
-        this.sections = settings.sections || [];
-      }
-    }
-  }
-}
---
+++ src/forestry.ts
@@ -0,0 +1,63 @@
+import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
+import * as yaml from "yamljs";
+import { EventEmitter } from "events";
+
+export namespace Forestry {
+  interface Section {
+    path: string;
+    label: string;
+    create: "all" | "documents" | "none";
+    templates?: string[];
+  }
+
+  export enum SiteEvents {
+    error = "error"
+  }
+
+  /**
+   * Represents the Site as understood by Forestry.
+   */
+  export class Site extends EventEmitter {
+    error: string | null = null;
+    sections: Section[] = [];
+
+    constructor(
+      private rootPath: string,
+      private forestryPath: string = ".forestry"
+    ) {
+      super();
+      this.loadForestrySettings();
+    }
+
+    sectionForUri(uri: vscode.Uri) {
+      const relPath = uri.path.replace(this.rootPath + "/", "");
+      const section = this.sections.find(section =>
+        relPath.startsWith(section.path)
+      );
+      if (section) {
+        return section;
+      }
+    }
+
+    private loadForestrySettings() {
+      try {
+        const settingsPath = path.join(
+          this.rootPath,
+          this.forestryPath,
+          "settings.yml"
+        );
+        const settingsFile = fs.readFileSync(settingsPath);
+        const settings = yaml.parse(settingsFile.toString());
+        this.sections = settings.sections || [];
+      } catch (e) {
+        // TODO: Distinguish bewteen missing & invalid
+        this.emit(
+          SiteEvents.error,
+          ".forestry/settings.yml not found or invalid"
+        );
+      }
+    }
+  }
+}
