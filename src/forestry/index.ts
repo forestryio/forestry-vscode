63c0b323cd213fc79e86ee248b79fc1725243b0d
ncphillips <ncphillips.19@gmail.com>
2018-07-09 08:27:50 -0300

    extracted out the Settings loader

--- src/forestry/index.ts
+++ src/forestry/index.ts
@@ -5,13 +5,6 @@ import * as yaml from "yamljs";
 import { EventEmitter } from "events";
 
 export namespace Forestry {
-  export interface Section {
-    path: string;
-    label: string;
-    create: "all" | "documents" | "none";
-    templates?: string[];
-  }
-
   export enum SiteEvents {
     error = "error"
   }
@@ -21,7 +14,10 @@ export namespace Forestry {
    */
   export class Site extends EventEmitter {
     error: string | null = null;
-    sections: Section[] = [];
+
+    settings: Settings = {
+      sections: []
+    };
 
     constructor(
       private rootPath: string,
@@ -31,26 +27,32 @@ export namespace Forestry {
       this.loadForestrySettings();
     }
 
-    sectionForUri(uri: vscode.Uri) {
+    /**
+     * The absolute path to the .forestry diretory.
+     */
+    get absForestryPath(): string {
+      return path.join(this.rootPath, this.forestryPath);
+    }
+
+    /**
+     * Given an vscode Uri, determine if the file is in a Forestry Section and return.
+     */
+    sectionForUri(uri: vscode.Uri): Section | null {
       const relPath = uri.path.replace(this.rootPath + "/", "");
-      const section = this.sections.find(section =>
+      const section = this.settings.sections.find(section =>
         relPath.startsWith(section.path)
       );
+
       if (section) {
         return section;
       }
+
+      return null;
     }
 
     private loadForestrySettings() {
       try {
-        const settingsPath = path.join(
-          this.rootPath,
-          this.forestryPath,
-          "settings.yml"
-        );
-        const settingsFile = fs.readFileSync(settingsPath);
-        const settings = yaml.parse(settingsFile.toString());
-        this.sections = settings.sections || [];
+        this.settings = Settings.load(this.absForestryPath);
       } catch (e) {
         // TODO: Distinguish bewteen missing & invalid
         this.emit(
@@ -58,6 +60,37 @@ export namespace Forestry {
           ".forestry/settings.yml not found or invalid"
         );
       }
+    }
+  }
+
+  /**
+   * The `.forestry/settings.yml` datastructure.
+   */
+  export interface Settings {
+    sections: Section[];
+  }
+
+  /**
+   * The data strucfture for Sections defined in the `.forestry/settings.yml`
+   */
+  export interface Section {
+    path: string;
+    label: string;
+    create: "all" | "documents" | "none";
+    templates?: string[];
+  }
+
+  /**
+   * Provides helper methods for interacting with the `.forestry/settings.yml`
+   */
+  export class Settings {
+    static DEFAULT: Settings = {
+      sections: []
+    };
+    static load(forestryPath: string) {
+      const settingsPath = path.join(forestryPath, "settings.yml");
+      const settingsFile = fs.readFileSync(settingsPath);
+      return { ...Settings.DEFAULT, ...yaml.parse(settingsFile.toString()) };
     }
   }
 }
